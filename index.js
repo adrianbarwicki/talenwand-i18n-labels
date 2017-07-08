9db281b0dac3e721fc43c60d31bfea3ea62dd0e9
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-07-08 18:21:22 +0200

    user properties added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     i18n: lang => require(`./i18n/${lang}.json`),
     config: () => require(`./config/config.json`),
+    userProperties: () => require(`./config/user-properties.json`),
     labelGroups: () => require(`./config/label-groups.json`)
 };
