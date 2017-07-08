0340c3e873a54177ff27b95dc1355366d8fa28da
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-07-08 15:18:49 +0200

    licence change to MIT, added label groups

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
     i18n: lang => require(`./i18n/${lang}.json`),
-    config: () => require(`./config/config.json`)
+    config: () => require(`./config/config.json`),
+    labelGroups: () => require(`./config/label-groups.json`)
 };
