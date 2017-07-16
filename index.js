3b22fa795d8f1fc7d45e03ad9d3db4a7a4a50ccf
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-07-16 16:26:49 +0200

    added initial posts

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     i18n: lang => require(`./i18n/${lang}.json`),
     config: () => require(`./config/config.json`),
     userProperties: () => require(`./config/user-properties.json`),
-    labelGroups: () => require(`./config/label-groups.json`)
+    labelGroups: () => require(`./config/label-groups.json`),
+    posts: () => require(`./config/posts.json`)
 };
