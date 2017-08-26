2c50fbed5f20722c2309ac40c156f2c8d44baa7e
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-08-26 16:58:15 +0200

    Added the concept of user verifications

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
     i18n: lang => require(`./i18n/${lang}.json`),
     config: () => require(`./config/config.json`),
     userProperties: () => require(`./config/user-properties.json`),
+    userVerifications: () => require(`./config/user-verifications.json`),
     labelGroups: () => require(`./config/label-groups.json`),
     posts: () => require(`./config/posts.json`)
 };
