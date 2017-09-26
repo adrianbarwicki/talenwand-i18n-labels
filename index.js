12a113ff2dbcf921f8b8e239e1d386a33cfe08ce
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-09-26 22:08:47 +0200

    error in paths

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 module.exports = {
     services: {
-        i18n: lang => require(`./i18n/${lang}.json`),
-        config: () => require(`./config/config.json`),
-        userProperties: () => require(`./config/user-properties.json`),
-        userVerifications: () => require(`./config/user-verifications.json`),
-        labelGroups: () => require(`./config/label-groups.json`),
-        posts: () => require(`./config/posts.json`)
+        i18n: lang => require(`./service-marketplace/i18n/${lang}.json`),
+        config: () => require(`./service-marketplace/config/config.json`),
+        userProperties: () => require(`./service-marketplace/config/user-properties.json`),
+        userVerifications: () => require(`./service-marketplace/config/user-verifications.json`),
+        labelGroups: () => require(`./service-marketplace/config/label-groups.json`),
+        posts: () => require(`./service-marketplace/config/posts.json`)
     },
     partners: {
-        i18n: lang => require(`./i18n/${lang}.json`),
-        config: () => require(`./config/config.json`),
-        userProperties: () => require(`./config/user-properties.json`),
-        userVerifications: () => require(`./config/user-verifications.json`),
-        labelGroups: () => require(`./config/label-groups.json`),
-        posts: () => require(`./config/posts.json`)
+        i18n: lang => require(`./partner-marketplace/i18n/${lang}.json`),
+        config: () => require(`./partner-marketplace/config/config.json`),
+        userProperties: () => require(`./partner-marketplace/config/user-properties.json`),
+        userVerifications: () => require(`./partner-marketplace/config/user-verifications.json`),
+        labelGroups: () => require(`./partner-marketplace/config/label-groups.json`),
+        posts: () => require(`./partner-marketplace/config/posts.json`)
     }
 };
