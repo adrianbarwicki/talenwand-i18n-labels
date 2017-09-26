0b447b77b32c5b04db49c3052235693cbf6e03e3
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-09-26 22:13:13 +0200

    adjustments in paths

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+const PATHS = {
+    SERVICES: './examples/service-marketplace',
+    PARTNERS: './examples/partner-marketplace'
+};
+
+const usecaseFactory = path => {
+    return {
+        i18n: lang => require(`${path}/i18n/${lang}.json`),
+        config: () => require(`${path}/config/config.json`),
+        userProperties: () => require(`${path}/config/user-properties.json`),
+        userVerifications: () => require(`${path}/config/user-verifications.json`),
+        labelGroups: () => require(`${path}/config/label-groups.json`),
+        posts: () => require(`${path}/config/posts.json`)
+    };
+};
+
 module.exports = {
-    services: {
-        i18n: lang => require(`./service-marketplace/i18n/${lang}.json`),
-        config: () => require(`./service-marketplace/config/config.json`),
-        userProperties: () => require(`./service-marketplace/config/user-properties.json`),
-        userVerifications: () => require(`./service-marketplace/config/user-verifications.json`),
-        labelGroups: () => require(`./service-marketplace/config/label-groups.json`),
-        posts: () => require(`./service-marketplace/config/posts.json`)
-    },
-    partners: {
-        i18n: lang => require(`./partner-marketplace/i18n/${lang}.json`),
-        config: () => require(`./partner-marketplace/config/config.json`),
-        userProperties: () => require(`./partner-marketplace/config/user-properties.json`),
-        userVerifications: () => require(`./partner-marketplace/config/user-verifications.json`),
-        labelGroups: () => require(`./partner-marketplace/config/label-groups.json`),
-        posts: () => require(`./partner-marketplace/config/posts.json`)
-    }
+    services: usecaseFactory(PATHS.SERVICES),
+    partners: usecaseFactory(PATHS.PARTNERS)
 };
